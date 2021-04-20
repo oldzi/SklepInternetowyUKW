2baa90b8b2f21dd33d27c7f8bfef13b02c3e56a6
Adrianna Piszcz <adrianna.piszcz@vp.pl>
2021-04-20 10:08:16 +0200

    Widok szczegoly

--- SklepUKW/SklepUKW/Controllers/FilmsController.cs
+++ SklepUKW/SklepUKW/Controllers/FilmsController.cs
@@ -29,11 +29,24 @@ namespace SklepUKW.Controllers
             return View(model);
         }
 
+        public ActionResult Details(int id)
+        {
+            var film = db.Films.Find(id);
+            return View(film);
+        }
+
         [ChildActionOnly]
         public ActionResult CategoriesMenu()
         {
             var categoryList = db.Categories.ToList();
             return PartialView("_CategoriesMenu", categoryList);
+        }
+
+        [ChildActionOnly]
+        public ActionResult FilmsFromCategory(string categoryName)
+        {
+            var category = db.Categories.Include("Films").Where(c => c.Name.ToLower() == categoryName.ToLower()).Single();
+            return PartialView("_FilmsFromCategory", category.Films.ToList());
         }
     }
 }
