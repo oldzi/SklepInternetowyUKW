579a7c3a79931e3af42fac1113b2f4a7a3c1fd77
Adrianna Piszcz <adrianna.piszcz@vp.pl>
2021-05-18 11:18:11 +0200

    Widok logowania i rejestracji Akcje logowania Modyfikacje akcji rejestracji

--- SklepUKW/SklepUKW/ViewModels/AccountViewModels.cs
+++ SklepUKW/SklepUKW/ViewModels/AccountViewModels.cs
@@ -8,11 +8,22 @@ namespace SklepUKW.ViewModels
 {
    public class LoginViewModel
    {
+        [EmailAddress(ErrorMessage = "Nieprawidłowy format adresu email")]
+        [Required(ErrorMessage = "Musisz wprowadzić login")]
+        public string Email { get; set; }
 
-   }
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        [Required(ErrorMessage = "Musisz wprowadzić hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+    }
 
     public class RegisterViewModel
     {
+        [EmailAddress(ErrorMessage = "Nieprawidłowy format adresu email")]
         [Required(ErrorMessage = "Musisz wprowadzić login")]
         public string Email { get; set; }
 
